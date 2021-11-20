be81b5df7e76cd62c5edcdc5a4ce837fcaf18b8b
Johan August Østbye <Johan.August@outlook.com>
2021-11-20 01:48:24 +0100

    put a opacity in the bg

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
   return (
-    <footer
-      className=' bg-overlay bg-opacity-elevation1 
+    <footer className='bg-surface w-full z-5'>
+      <div
+        className=' bg-overlay bg-opacity-elevation1 
      text-on-background flex items-center justify-center w-full h-24'
-    >
-      <hr />
-      <span>foot</span>
+      >
+        <hr />
+        <span>foot</span>
+      </div>
     </footer>
   );
 };
--- components/elements/Hero.tsx
+++ components/elements/Hero.tsx
@@ -6,8 +6,14 @@ const Hero = () => {
   return (
     <div id='home'>
       <div className='absolute inset-0 w-full h-full overflow-hidden'>
-        <Image src={hero} layout='fill' objectFit='cover' />
+        <Image
+          src={hero}
+          layout='fill'
+          objectFit='cover'
+          className=' opacity-20'
+        />
       </div>
+      <div>hello</div>
     </div>
   );
 };
