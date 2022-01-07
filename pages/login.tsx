4404033e80a5192e077c6945fb29053e7ae605c5
Johan August Østbye <johan.august@outlook.com>
2022-01-07 03:11:26 +0100

    started on signup and loginpages

---
+++ pages/login.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const LoginPage = () => {
+  return (
+    <Layout title='About | Next.js + TypeScript Example'>
+      <section className='flex items-center justify-center bg-hero-pattern bg-no-repeat bg-center w-full min-h-[calc(100vh-64px)]'>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit pariatur
+          neque alias commodi corporis quaerat impedit, asperiores, soluta
+          maiores iste maxime et adipisci fugiat libero labore laborum iusto
+          corrupti ad.
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default LoginPage;
